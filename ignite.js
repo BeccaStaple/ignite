7995f10a0f6347fdef4597c98eff0e2ab152cfd7
BeccaStaple <56489791+BeccaStaple@users.noreply.github.com>
2020-03-21 11:52:55 +0000

    added contatc modal

---
+++ ignite.js
@@ -0,0 +1,12 @@
+
+//contact click open
+
+function openContact() {
+    document.getElementById("contactModalId").style.display = "block"
+}
+
+//contact close
+
+function closeContactModal() {
+    document.getElementById("contactModalId").style.display = "none"
+}
